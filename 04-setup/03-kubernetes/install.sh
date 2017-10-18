a7b55a46351a7eefd808177804495ddbc8812d11
Georg Kunz <kwd@gmx.ch>
2017-10-18 15:21:42 +0200

    Fix typo in installer

--- 04-setup/03-kubernetes/install.sh
+++ 04-setup/03-kubernetes/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p /etc/kubernetes/manifests
-mkidr -p /var/lib/etcd
+mkdir -p /var/lib/etcd
 
 echo "Download and install Kubelet"
 wget -q -O /usr/bin/kubelet https://storage.googleapis.com/kubernetes-release/release/v1.7.8/bin/linux/amd64/kubelet
